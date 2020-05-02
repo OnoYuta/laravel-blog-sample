7d81aca54c9bf21e7ee9c7f5791933fe13eab3bc
ono <f25jzzydg@me.com>
2020-05-03 08:50:17 +0900

    feat user add username column

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,9 +1,9 @@
 <?php
 
 use App\User;
-use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -17,6 +17,12 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table($this->table)->truncate();
+
+        factory(User::class)->create([
+            'email'       => 'user',
+            'email'       => 'user@example.com',
+            'password'    => Hash::make('password'),
+        ]);
 
         factory(User::class, 100)->create();
     }
