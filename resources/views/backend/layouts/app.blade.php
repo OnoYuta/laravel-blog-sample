61c5cac5b72f4ba5f062737846a386d9045a45b5
ono <f25jzzydg@me.com>
2020-05-04 10:12:19 +0900

    feat create E2E Admin Auth test

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -57,7 +57,7 @@
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('admin.logout') }}" onclick="event.preventDefault();
+                                <a class="dropdown-item" dusk="logoutLink" href="{{ route('admin.logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
                                 </a>
