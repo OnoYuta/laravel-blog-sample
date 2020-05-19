f703c830fbd28c5c9d4de72216eef5073115614a
ono <f25jzzydg@me.com>
2020-05-19 21:11:04 +0900

    fix DatabaseSeeder to remove if branch

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,16 +13,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         //シーダーを実行する前に、この接続の外部キーチェックを無効にする
-        if (config('database.default') == 'mysql') {
-            DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        }
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 
         $this->call(UsersTableSeeder::class);
         $this->call(AdministratorsTableSeeder::class);
 
         // 外部キーチェックを明示的に有効に戻す（これを省略しても接続が終了すれば元に戻る）
-        if (config('database.default') == 'mysql') {
-            DB::statement('SET FOREIGN_KEY_CHECKS=1;');
-        }
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
