40e539ce172b2bf462145413526633faa8a47baa
ono <f25jzzydg@me.com>
2020-05-22 20:43:34 +0900

    feat Post model create and fix docs

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'administrator_id',
+        'title',
+        'contents',
+        'status',
+        'published_at',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
 }
