fd3103ad5ef6671447c0783de8781f7360e151e7
ono <f25jzzydg@me.com>
2020-05-23 23:45:16 +0900

    feat create post validation

--- app/Http/Controllers/Backend/Admin/PostController.php
+++ app/Http/Controllers/Backend/Admin/PostController.php
@@ -7,6 +7,8 @@ use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class PostController extends AdminController
 {
@@ -77,11 +79,31 @@ class PostController extends AdminController
     {
         $form = new Form(new Post());
 
-        $form->number('administrator_id', __('fields.Administrator id'));
-        $form->text('title', __('fields.Title'));
-        $form->text('contents', __('fields.Contents'));
-        $form->text('status', __('fields.Status'));
-        $form->datetime('published_at', __('fields.Published at'))->default(date('Y-m-d H:i:s'));
+        $form->hidden('administrator_id', __('fields.Administrator id'))
+            ->value(Auth::guard('admin')->user()->getAttribute('id'));
+        $form->text('title', __('fields.Title'))
+            ->required();
+        $form->text('contents', __('fields.Contents'))
+            ->required();
+        $form->select('status', __('fields.Status'))
+            ->options(collect(config('fields.post_status'))->mapWithKeys(function ($value) {
+                return [$value => trans('fields.' . $value)];
+            }))
+            ->default('draft')
+            ->rules([
+                'bail',
+                'required',
+                Rule::in(config('fields.post_status')),
+            ]);
+        $form->datetime('published_at', __('fields.Published at'))
+            ->default(date('Y-m-d H:i:s'))
+            ->rules([
+                'bail',
+                'required',
+                'after_or_equal:today',
+            ], [
+                'after_or_equal' => '公開日には、本日以降の日付を指定してください。'
+            ]);
 
         return $form;
     }
