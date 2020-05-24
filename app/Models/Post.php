03b3ae898c15a16875ee399f3d963863a064a787
ono <f25jzzydg@me.com>
2020-05-24 20:53:58 +0900

    feat PostController show parsed markdown as preview

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use cebe\markdown\Markdown;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
@@ -36,5 +37,17 @@ class Post extends Model
     public function administrator()
     {
         return $this->belongsTo(Administrator::class);
+    }
+
+    /**
+     * Get parsed maekup contents.
+     *
+     * @return void
+     */
+    public function getHtmlAttribute()
+    {
+        $parsedHtml = (new Markdown())->parse($this->contents);
+
+        return $parsedHtml;
     }
 }
