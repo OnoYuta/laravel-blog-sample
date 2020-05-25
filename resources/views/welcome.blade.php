77d7d4a848a9ca5f11dc57e8eee3ffb72d3da961
ono <f25jzzydg@me.com>
2020-05-25 22:18:49 +0900

    create WelcomePage navigation bar style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,20 +24,37 @@
 
 <body>
     <div id='app'>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-            <div class="top-right links">
-                @auth
-                <a href="{{ url()->to('home') }}">{{ __('Home') }}</a>
-                @else
-                <a href="{{ url()->to('login') }}">{{ __('Login') }}</a>
-
-                @if (Route::has('register'))
-                <a href="{{ url()->to('register') }}">{{ __('Register') }}</a>
-                @endif
-                @endauth
+        <!-- nav-bar -->
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+            <div class="container">
+                <a href="{{ url('/') }}" class="navbar-brand"><i class="fas fa-braille"></i> {{ config('app.name', 'Laravel') }}</a>
+                <button class="navbar-toggler" data-toggle="collapse" data-target="#mainNav">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="mainNav">
+                    <ul class="navbar-nav ml-auto">
+                        @if (Route::has('login'))
+                        @auth
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url()->to('home') }}">{{ __('Home') }}</a>
+                        </li>
+                        @else
+                        <li class="nav-item ml-auto">
+                            <a class="nav-link" href="{{ url()->to('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                        <li class="nav-item ml-auto">
+                            <a class="nav-link" href="{{ url()->to('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
+                        @endauth
+                        @endif
+                        <li class="nav-item">
+                    </ul>
+                </div>
             </div>
-            @endif
+        </nav>
+        <div class="flex-center position-ref full-height">
 
             <div class="content">
                 <div class="title m-b-md">
