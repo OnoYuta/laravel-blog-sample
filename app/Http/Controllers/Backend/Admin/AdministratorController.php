31c72b9804b82f8e0cce5ef06a6cfbf6d46a5d26
ono <f25jzzydg@me.com>
2020-05-31 21:10:46 +0900

    Fix AdministratorController to add administrator role at saved

--- app/Http/Controllers/Backend/Admin/AdministratorController.php
+++ app/Http/Controllers/Backend/Admin/AdministratorController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers\Backend\Admin;
 
-use App\Models\Administrator;
+use App\Models\LaravelAdmin\Administrator;
 use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
+use Illuminate\Support\Facades\Hash;
 
 class AdministratorController extends AdminController
 {
@@ -33,6 +34,8 @@ class AdministratorController extends AdminController
 
         $grid->column('id', __('fields.Id'));
         $grid->column('name', __('fields.Name'));
+        // @phpstan-ignore-next-line
+        $grid->column('roles', trans('admin.roles'))->pluck('name')->label();
         $grid->column('username', __('fields.Username'));
         $grid->column('email', __('fields.Email'));
         $grid->column('created_at', __('fields.Created at'));
@@ -62,6 +65,9 @@ class AdministratorController extends AdminController
 
         $show->field('id', __('fields.Id'));
         $show->field('name', __('fields.Name'));
+        $show->field('roles', trans('admin.roles'))->as(function ($roles) {
+            return $roles->pluck('name');
+        })->label();
         $show->field('username', __('fields.Username'));
         $show->field('email', __('fields.Email'));
         $show->field('created_at', __('fields.Created at'));
@@ -77,13 +83,36 @@ class AdministratorController extends AdminController
      */
     protected function form()
     {
+        $permissionModel = config('admin.database.permissions_model');
+        $roleModel = config('admin.database.roles_model');
+
         $form = new Form(new Administrator());
 
+        $form->display('id', 'ID');
         $form->text('name', __('fields.Name'))->rules('required|string|between:1,20');
         $form->text('username', __('fields.Username'))->rules('required|string|between:1,20');
         $form->email('email', __('fields.Email'))->rules('required|email');
         $form->password('password', __('fields.Password'))->rules('required|string|alpha_dash|confirmed|between:6,20');
-        $form->password('password_confirmation', __('fields.Password Confirmation'));
+        $form->password('password_confirmation', __('fields.Password Confirmation'))->rules('required')
+            ->default(function ($form) {
+                return $form->model()->password;
+            });
+        $form->ignore(['password_confirmation']);
+
+        $form->multipleSelect('roles', trans('admin.roles'))
+            ->options($roleModel::all()->pluck('name', 'id'))
+            ->default($roleModel::where('name', 'Administrator')->first()->id)
+            ->readonly();
+
+        $form->display('created_at', trans('admin.created_at'));
+        $form->display('updated_at', trans('admin.updated_at'));
+
+        $form->saving(function (Form $form) {
+            // @phpstan-ignore-next-line
+            if ($form->password && $form->model()->password != $form->password) {
+                $form->password = Hash::make($form->password);
+            }
+        });
 
         return $form;
     }
