709c4bc7608761e11ba8e5ac8480bb5804ff0245
ono <f25jzzydg@me.com>
2020-05-31 09:23:37 +0900

    Fix Welcome blade markup

--- resources/views/frontend/includes/header.blade.php
+++ resources/views/frontend/includes/header.blade.php
@@ -3,14 +3,16 @@
         <p class="site-title-sub display-5">Sample blog project featuring many basic and general topics.</p>
         <h1 class="d-md-4 display-2 site-title">{{ config('app.name', 'Laravel') }}</h1>
         <p class="site-description display-5">Check out come of my works.</p>
-        <div class="row w-100 d-flex justify-content-center">
-            <div class="col-4 col-sm-3 col-md-2">
+        <div class="btn-group row w-100 justify-content-center" role="group">
+            <div class="col-xl-3 col-lg-4 col-md-5 col-12 mb-2 text-md-right">
                 {{Form::open(['route' => 'login'])}}
                 {{Form::hidden('username', config('account.sample.user.username'))}}
                 {{Form::hidden('password', config('account.sample.user.password'))}}
-                {{Form::submit(__('Login With Sample Account'), ['class' => 'btn btn-primary', 'dusk' => 'sampleLoginBtn'])}}
+                {{Form::submit(__('Login With Sample Account'), ['class' => 'btn btn-primary col', 'dusk' => 'sampleLoginBtn'])}}
             </div>
-            <a href="{{ route('login') }}" class="btn btn-secondary offset-1 col-4 col-sm-3 col-md-2">ログインフォームを開く</a>
+            <div class="col-xl-3 col-lg-4 col-md-5 col-12 text-md-left">
+                <a href="{{ route('login') }}" class="btn btn-secondary col">{{ __('Show Login Form') }}</a>
+            </div>
         </div>
     </div>
 </header>
